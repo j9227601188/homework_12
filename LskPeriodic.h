e646b02fde824ea6bc0fbe376ca1c48b4e1ac951
Jon <118042236+j9227601188@users.noreply.github.com>
2023-02-24 02:07:11 +0300

    Create LskPeriodic.h

---
+++ LskPeriodic.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+//checking the multiplicity of a string to the number K
+auto lskPeriodic(const std::string& in_string, const int K) {
+	//exception
+	if (K <= 0) {
+		std::cout << K << " invalid divisor" << "\n";
+		return false;
+	}
+	//check
+	if (in_string.size() % K == 0) {
+		for (size_t i{ 0 }; i < K; ++i) {
+			for (size_t j{ i + K }; j < in_string.size(); j += K) {
+				if (in_string[i] != in_string[j]) {
+					std::cout << in_string << " not a multiple " << K << "\n";
+					return false;
+				}
+			}
+		}
+		std::cout << in_string << " multiple " << K << "\n";
+		return true;
+	}
+	//invalid
+	std::cout << in_string << " not a multiple " << K << "\n";
+	return false;
+};
