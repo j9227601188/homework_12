51862c8c4848cc538ea9a6ad19175dc1b187637d
Jon <118042236+j9227601188@users.noreply.github.com>
2023-02-24 02:21:31 +0300

    Update TestScript.h

--- TestScript.h
+++ TestScript.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "LskPeriodic.h"
+#include "IsKPeriodic.h"
 
 void testScript() {
 	std::string test_str_01{ "qweqweqweqweqweqweqweqwe" };
 	int test_int_00{ 0 }, test_int_01{ -1 }, test_int_02{ 2 }, test_int_03{ 3 }, test_int_100{ 100 };
 	std::cout << "Output of autotesting: \n";
-	lskPeriodic(test_str_01, test_int_00);
-	lskPeriodic(test_str_01, test_int_01);
-	lskPeriodic(test_str_01, test_int_02);
-	lskPeriodic(test_str_01, test_int_03);
-	lskPeriodic(test_str_01, test_int_100);
+	isKPeriodic(test_str_01, test_int_00);
+	isKPeriodic(test_str_01, test_int_01);
+	isKPeriodic(test_str_01, test_int_02);
+	isKPeriodic(test_str_01, test_int_03);
+	isKPeriodic(test_str_01, test_int_100);
 }
