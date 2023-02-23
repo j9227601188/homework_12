cf7494e9f6b626753d2ad5c2dafb3c5c057fc981
Jon <118042236+j9227601188@users.noreply.github.com>
2023-02-24 02:08:22 +0300

    Create TestInManualMode.h

---
+++ TestInManualMode.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "LskPeriodic.h"
+
+void testInManualMode() {
+	std::cout	<< "\nTo test the lskPeriodic() function, press enter 1 or 0 to EXIT\n";
+	int of{ 0 };
+	std::cin >> of;
+	while (of) {
+		std::cout << "Enter a text for testing: ";
+		std::string userString;
+		std::cin.ignore();
+		getline(std::cin, userString);
+		std::cout << userString << "\n";//del
+		std::cout << "Enter a multiplicity for testing: ";
+		size_t userMultiplicity{ 0 };
+		std::cin >> userMultiplicity;
+		std::cout << userMultiplicity << "\n";//del
+		lskPeriodic(userString, userMultiplicity);
+		std::cout << "to repeat the test, enter 1 or 0 to EXIT\n";
+		std::cin >> of;
+	}
+}
